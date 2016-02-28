6c04bb106d78d3d28624c3915bb2eaa8470f6835
Leah Churchley <leah.churchley@gmail.com>
2016-02-28 11:55:30 -0800

    Adding JS/JQ

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$("li").click(function(){
+		$(this).toggleClass("done");
+	});
+
+	$("img").click(function(){
+		$(this).toggleClass("fade");
+	});
+
+
+
+
+
+
+
+
+});
